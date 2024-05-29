1a63287e43156e806e5a77f06e23689bb6922d41
RrRun0216 <144758468+RrRun0216@users.noreply.github.com>
2024-05-29 12:10:55 +0800

    Delete week15-1.py

--- week15-1.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        N = len(s)
-        for i in range(N):
-            print( s[i], t[i] )
-            return 0
